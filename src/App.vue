cfa3d5a3d00c83131d7c8d6475b776c5e3f80ec8
MarvellChristian <marvellchristian1613@gmail.com>
2026-07-29 23:02:48 +0700

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header />
+</template>
+
+<script setup lang="ts">
+  import Header from './components/Header.vue';
+</script>
+
+<style>
+  @import "tailwindcss";
+  @import url('https://fonts.googleapis.com/css2?family=Alata&display=swap');
+  
+  body {
+    font-family: 'Alata', sans-serif;
+  }
+
+  .background-black {
+    background-color: #00171F;
+  }
+
+  .font-white {
+    color: #E2E2E2;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="flex justify-between items-center pt-8 pb-8 pr-20 pl-20 background-black">
+    <img
+      src="./../../public/logo-white.png"
+      alt="logo"
+      class="w-12 h-12"
+    >
+
+    <nav class="flex gap-16">
+      <a href="#home" class="text-2xl font-white">Home</a>
+      <a href="#about" class="text-2xl font-white">About</a>
+      <a href="#projects" class="text-2xl font-white">Projects</a>
+      <a href="#contact" class="text-2xl font-white">Contact</a>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
