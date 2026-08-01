91097054b0a01998d46ad5fb4fb880e0919a36fb
MarvellChristian <marvellchristian1613@gmail.com>
2026-08-01 13:09:54 +0700

    Fix header behavior

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
     >
 
     <nav class="flex gap-16 text-white">
-      <a href="#home" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Home</a>
+      <a href="#home" :class="['text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out', { 'text-blue-400': activeSection === 'home' }]">Home</a>
       <a href="#about" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">About</a>
       <a href="#projects" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Projects</a>
       <a href="#contact" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Contact</a>
@@ -16,4 +16,33 @@
 </template>
 
 <script setup lang="ts">
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+  const activeSection = ref('home')
+
+  const handleScroll = () => {
+    const sections = ['home', 'about', 'projects', 'contact']
+    const scrollPosition = window.scrollY + 100 // 100 is the number of the approx Header's height
+
+    for (const section of sections) {
+      const element = document.getElementById(section)
+      if (element) {
+        const { offsetTop, offsetHeight } = element
+        if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+          console.log(element)
+          activeSection.value = section
+          break
+        }
+      }
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', handleScroll)
+    handleScroll()
+  })
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll)
+  })
 </script>
--- src/components/sections/Landing.vue
+++ src/components/sections/Landing.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="home" class="flex justify-between h-screen">
     <div class="flex w-full self-end justify-center gap-20">
-      <div class="font-white max-w-150 pt-30">
+      <div class="font-white max-w-150 pt-25">
         <p class="text-2xl mb-2">Hi, my name is</p>
-        <h1 class="text-4xl mb-10">MARVELL CHRISTIAN WIDYASANA</h1>
+        <h1 class="text-5xl/15 mb-10">MARVELL CHRISTIAN WIDYASANA</h1>
         <p class="text-lg mb-12">I'm a Software Engineer passionate about crafting intuitive user experiences and building modern full-stack applications. I enjoy turning ideas into clean, scalable, and impactful digital products.</p>
         <button
           class="w-[160px] h-[40px] bg-blue-500 text-white rounded-md cursor-pointer hover:bg-blue-700"
