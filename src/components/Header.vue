658398b1f6760beb2434cab6d6d8599774e259c1
MarvellChristian <marvellchristian1613@gmail.com>
2026-08-01 12:47:57 +0700

    Fix landing section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex w-full justify-between items-center pt-6 pb-6 pr-20 pl-20 background-black fixed">
+  <header class="flex w-full justify-between items-center pt-6 pb-6 pr-20 pl-20 background-black fixed z-100">
     <img
       src="/logo-white.png"
       alt="logo"
@@ -7,10 +7,10 @@
     >
 
     <nav class="flex gap-16 text-white">
-      <a href="#home" class="text-2xl">Home</a>
-      <a href="#about" class="text-2xl">About</a>
-      <a href="#projects" class="text-2xl">Projects</a>
-      <a href="#contact" class="text-2xl">Contact</a>
+      <a href="#home" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Home</a>
+      <a href="#about" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">About</a>
+      <a href="#projects" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Projects</a>
+      <a href="#contact" class="text-2xl hover:text-blue-400 transition-colors duration-200 ease-in-out">Contact</a>
     </nav>
   </header>
 </template>
--- src/components/sections/Landing.vue
+++ src/components/sections/Landing.vue
@@ -1,14 +1,9 @@
 <template>
   <section id="home" class="flex justify-between h-screen">
     <div class="flex w-full self-end justify-center gap-20">
-      <img
-        class="w-96"
-        src="/profile-pic-1.png"
-        alt="Profile Picture"
-      >
       <div class="font-white max-w-150 pt-30">
         <p class="text-2xl mb-2">Hi, my name is</p>
-        <h1 class="text-4xl mb-10">Marvell Christian Widyasana</h1>
+        <h1 class="text-4xl mb-10">MARVELL CHRISTIAN WIDYASANA</h1>
         <p class="text-lg mb-12">I'm a Software Engineer passionate about crafting intuitive user experiences and building modern full-stack applications. I enjoy turning ideas into clean, scalable, and impactful digital products.</p>
         <button
           class="w-[160px] h-[40px] bg-blue-500 text-white rounded-md cursor-pointer hover:bg-blue-700"
@@ -16,6 +11,11 @@
           Contact Me
         </button>
       </div>
+      <img
+        class="w-96"
+        src="/profile-pic-1.png"
+        alt="Profile Picture"
+      >
     </div>
   </section>
 </template>
